b8481960665e2dc244e6cb909b6305834c673f1c
☃️ Ohlalapoy 💕 <ploythanarat@hotmail.co.uk>
2026-03-28 16:19:10 +0700

    add more comment

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "allow_web" {
   egress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
+    protocol         = "-1" #allow network protocol
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
